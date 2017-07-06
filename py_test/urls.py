3ad8d0a55f3448bc441d9ee591b569cf87ae1170
ptera-py <kurzy@yandex.ru>
2017-07-06 06:21:44 +0300

    main stranitsa

--- py_test/urls.py
+++ py_test/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    url(r'^$', views.huindex, name='huindex'),
     url(r'^index/', include('index.urls')),
     url(r'^admin/', admin.site.urls),
 ]
